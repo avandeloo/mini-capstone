d0f67be25abbf50bcdb8875ed288de51e44ed3aa
avandeloo <anthony.vandeloo@gmail.com>
2017-01-17 20:01:03 -0600

    added a page that displays one product and a page that displays all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def one_product
+    @product = Product.first
+  end
+
+  def all_products
+    @product = Product.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/one_product_url' => 'products#one_product'
+  get '/all_products_url' => 'products#all_products'
 end
