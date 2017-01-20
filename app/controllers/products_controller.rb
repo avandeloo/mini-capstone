58ab373fa229c758c46c007b328579d8c567865a
avandeloo <anthony.vandeloo@gmail.com>
2017-01-19 20:04:01 -0600

    added ability to add products from user input

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def all_products
     @product = Product.all
   end
+
+  def add_new_product_form
+  end
+
+  def add_new_product_result
+    @product = Product.new(
+                          name: params["name"],
+                          price: params["price"],
+                          image: params["image"],
+                          description: params["description"],
+                          release_date: params["release_date"]
+
+                        )
+    @product.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/one_product_url' => 'products#one_product'
   get '/all_products_url' => 'products#all_products'
+  get '/add_product_url' => 'products#add_new_product_form'
+  post '/add_product_result_url' => 'products#add_new_product_result'
 end
