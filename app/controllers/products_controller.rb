9847aa48faebd96276e21fd09ca1f12ee1073fd5
avandeloo <anthony.vandeloo@gmail.com>
2017-01-22 09:38:39 -0600

    added new and create pages to the mini-capstone app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,19 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    
+  end
+
+  def create
+    @product = Product.new(
+                        name: params[:name],
+                        price: params[:price],
+                        image: params[:image],
+                        description: params[:description],
+                        release_date: params[:release_date]
+                        )
+    @product.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'products#index'
   get '/products' => 'products#index'
+
+  get 'products/new' => 'products#new'
+  post '/products' => 'products#create'
+
   get '/products/:id' => 'products#show'
 
 end
