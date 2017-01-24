420d40901f7c503e08ddaeacd2276411596ce757
avandeloo <anthony.vandeloo@gmail.com>
2017-01-24 15:33:56 -0600

    added sale_message, tax, and total methods to product model and improved css

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def sale_message
+    message = "Discount Item!" if price < 2
+    message = "Everyday Value!!" if price > 2
+    message
+  end
+
+  def tax
+    @tax = price * 0.09
+    sprintf("%.2f", @tax.round(2))
+  end
+
+  def total
+    total = price + @tax
+    sprintf("%.2f", total.round(2))
+  end
 end
